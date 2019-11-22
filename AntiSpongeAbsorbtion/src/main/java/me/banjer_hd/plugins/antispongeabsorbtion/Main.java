182ebc842ec51b32c2eded82a0bef7c45b5ddcf8
Banjer_HD <siem.lohuis@gmail.com>
2019-11-22 15:54:22 +0100

    Added world support

--- AntiSpongeAbsorbtion/src/main/java/me/banjer_hd/plugins/antispongeabsorbtion/Main.java
+++ AntiSpongeAbsorbtion/src/main/java/me/banjer_hd/plugins/antispongeabsorbtion/Main.java
@@ -7,9 +7,27 @@ import me.banjer_hd.plugins.antispongeabsorbtion.events.OnSpongeAbsorbtion;
 
 public class Main extends JavaPlugin {
 	
+	private static Main instance;
+	
 	@Override
 	public void onEnable() {
+		instance = this;
+		
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		
 		Bukkit.getPluginManager().registerEvents(new OnSpongeAbsorbtion(), this);
+	}
+	
+	@Override
+	public void onDisable() {
+		instance = null;
+	}
+	
+	
+	
+	public static Main getInstance() {
+		return instance;
 	}
 	
 }
--- AntiSpongeAbsorbtion/src/main/java/me/banjer_hd/plugins/antispongeabsorbtion/events/OnSpongeAbsorbtion.java
+++ AntiSpongeAbsorbtion/src/main/java/me/banjer_hd/plugins/antispongeabsorbtion/events/OnSpongeAbsorbtion.java
@@ -4,11 +4,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SpongeAbsorbEvent;
 
+import me.banjer_hd.plugins.antispongeabsorbtion.Main;
+
 public class OnSpongeAbsorbtion implements Listener {
 	
 	@EventHandler
 	public void onEvent(SpongeAbsorbEvent e) {
-		e.setCancelled(true);
+		if(Main.getInstance().getConfig().getList("enabled_worlds").contains("all") || Main.getInstance().getConfig().getList("enabled_worlds").contains(e.getBlock().getWorld().getName())) {
+			e.setCancelled(true);
+		}
 	}
 	
 }
